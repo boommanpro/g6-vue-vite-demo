9a9904c34ec8599b7553eff6deb3b7a54609c0bc
boommanpro <boommanpro@gmail.com>
2025-04-01 21:33:51 +0800

    feat(init):

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: './', // 设置基础路径为 './' 表示相对路径
+})
